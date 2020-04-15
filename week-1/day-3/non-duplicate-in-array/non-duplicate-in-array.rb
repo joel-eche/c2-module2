86692f8abb408455aee8cfa44e98d0991371bf1a
Joel Eche <joel.eche5@gmail.com>
2020-04-15 18:52:26 -0500

    Solution using sort

--- week-1/day-3/non-duplicate-in-array/non-duplicate-in-array.rb
+++ week-1/day-3/non-duplicate-in-array/non-duplicate-in-array.rb
@@ -1,7 +1,24 @@
 def non_duplicated_values(values)
-  values.select {|element| values.count(element) == 1}
+  result = []
+  values.sort!
+
+  values.each do |element|
+    unless result.include?(element)
+      result << element if values.count(element) == 1
+    end
+  end
+
+  result
 end
 
 # test
 p non_duplicated_values([1,2,2,3,3,4,5]) #returns [1,4,5]
 p non_duplicated_values([1,2,2,3,4,4]) #returns [1,3]
+
+
+=begin
+# another way to resolve it
+def non_duplicated_values(values)
+  values.select {|element| values.count(element) == 1}
+end
+=end
