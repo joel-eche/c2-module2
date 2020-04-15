76ea464e16be39f7531c9202415cab2ad9e0de3b
Joel Eche <joel.eche5@gmail.com>
2020-04-15 02:39:12 -0500

    Solution to the <Count words> assignment

---
+++ week-1/day-2/count-words/count-words.rb
@@ -0,0 +1,11 @@
+def count_words(text)
+  text.downcase!
+  words = text.split(" ")
+  result = Hash.new(0)
+
+  words.each { |word| result[word] += 1 }
+  result
+end
+
+# test
+puts count_words("cake cake caKe I like cake very much please gIve me cake I need the cake i need it")
